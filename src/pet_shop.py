e8c5f106d71bba68b9e4fd4224371cea349d4ee6
justin reid <hi@justinre.id>
2022-07-30 18:17:28 +0100

    completed find/remove

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -23,3 +23,24 @@ def increase_pets_sold(pets, pets_increase):
 def get_stock_count(stock):
     return len(stock["pets"])
 
+def get_pets_by_breed(pets, bsh):
+    bsh_list = []
+    for pet in pets["pets"]:
+        if pet["breed"] == bsh:
+            bsh_list.append(bsh)
+    return bsh_list
+
+def find_pet_by_name(pets, name):
+# navigate to pets list
+    for pet in pets["pets"]:
+        if pet["name"] == name:
+            return pet
+
+
+def remove_pet_by_name(pets, name):
+# iterate through list to find arthur
+    for pet in pets["pets"]:
+        if pet["name"] == name:
+            pets["pets"].remove(pet)
+
+    
