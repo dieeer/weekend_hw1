86415a17e065fa2b7a9a4e19fc64d3fa5a2418f6
justin reid <hi@justinre.id>
2022-07-30 16:00:40 +0100

    add_or_remove completed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -3,6 +3,13 @@
 def get_pet_shop_name(name):
     return name["name"]
 
-def get_total_cash(sum):
-    return sum["admin"]["total_cash"]
+def get_total_cash(cash):
+    return cash["admin"]["total_cash"]
 
+def add_or_remove_cash(cash, addition):
+    cash["admin"]["total_cash"] += addition
+    return addition
+
+def add_or_remove_cash(cash, subtraction):
+    cash["admin"]["total_cash"] += subtraction
+    return subtraction
