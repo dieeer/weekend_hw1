5708133951fc02bdeb51954b692a8c5f4996ff37
justin reid <hi@justinre.id>
2022-07-30 16:13:34 +0100

    get_stock_count complete

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -13,3 +13,13 @@ def add_or_remove_cash(cash, addition):
 def add_or_remove_cash(cash, subtraction):
     cash["admin"]["total_cash"] += subtraction
     return subtraction
+
+def get_pets_sold(pets):
+    return pets["admin"]["pets_sold"]
+
+def increase_pets_sold(pets, pets_increase):
+    pets["admin"]["pets_sold"] += pets_increase
+
+def get_stock_count(stock):
+    return len(stock["pets"])
+
